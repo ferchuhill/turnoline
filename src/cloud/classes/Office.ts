b4a079848c7375c9c0c66e776040829c6c0f1c1d
Fernando Andrés Hillebrand <fer.hill@gmail.com>
2021-03-10 08:04:17 -0300

    reset

--- src/cloud/classes/Office.ts
+++ src/cloud/classes/Office.ts
@@ -1,20 +1,8 @@
 import Base from './Base';
-
 class Office extends Base {
-    constructor() {
-      super(Office.prototype.constructor.name);
-    }
-
-    static async afterSave(request: Parse.Cloud.AfterSaveRequest) {
-        const { object, user } = request;
-        if (user && !object.has('defaultRole')) {
-          const query = new Parse.Query('Zone');
-          const roleACL = new Parse.ACL();
-          roleACL.setPublicReadAccess(true);
-          const role = new Parse.Role(`ROLE_${Organization.name.toUpperCase()}_${object.id}`, roleACL);
-          await role.save();
-          object.set('defaultRole', role);
-          await object.save(null, { sessionToken: user.getSessionToken() });
-        }
-      }
+  constructor() {
+    super(Office.name);
+  }
 }
+
+export default Office;
